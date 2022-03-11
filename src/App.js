80c44681786645b41d2ad3efa5f2ce8bca3e3cc1
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-11 07:46:16 -0300

    chore: add route path /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
+import React from "react";
 
 let App = () => {
   return (
     <React.Fragment>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/contacts/list" />
+      </Routes>
     </React.Fragment>
   );
 };
