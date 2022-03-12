416b67b3abfc2f65221f72cf02385a647c6ef262
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-11 22:58:53 -0300

    chore: import Navigate and add path to ContactList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes , Route, Navigate} from "react-router-dom";
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import ContactList from "./components/contacts/ContactList/ContactList";
 
 let App = () => {
   return (
     <React.Fragment>
       <NavBar />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/contacts/list" element={<NavBar/>}/>
+        <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}/>
+        <Route path={'/contacts/list'} element={<ContactList/>}/>
       </Routes>
     </React.Fragment>
   );
