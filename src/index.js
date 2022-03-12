ed349df5154a5e12f0945b2edc2ffaa7748c31c8
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-11 22:59:42 -0300

    fix: remove node_modules/fontawesome

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
