8ebc9227d64e763642aca1deeb043f22930c0873
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-11 23:20:34 -0300

    feat: add url path for AddContact

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
-import { Routes , Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import ContactList from "./components/contacts/ContactList/ContactList";
+import AddContact from "./components/contacts/AddContact/AddContact";
 
 let App = () => {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <Routes>
-        <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}/>
-        <Route path={'/contacts/list'} element={<ContactList/>}/>
-      </Routes>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <NavBar/>
+            <Routes>
+                <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}/>
+                <Route path={'/contacts/list'} element={<ContactList/>}/>
+                <Route path={'/contacts/add'} element={<AddContact/>}/>
+            </Routes>
+        </React.Fragment>
+    );
 };
 
 export default App;
