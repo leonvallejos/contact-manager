69b34e4d8e61d60983057a462fb7ba21c689a66a
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-12 09:11:00 -0300

    feat: add search and form

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -11,10 +11,22 @@ let ContactList = () => {
                     <div className="col">
                         <p className="h3">Contact Manager
                             <Link to={'/contacts/add'} className="btn btn-primary ms-2">
-                                <i className="fa fa-plus-circle ms-2"/>
-                                New</Link>
+                                <i className="fa fa-plus-circle me-2"/>
+                                 New</Link>
                         </p>
                         <p className="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium alias aliquam corporis delectus distinctio, dolores eveniet expedita fugiat iusto labore magnam, molestiae quae recusandae tempora vel veniam? Iure, ut.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <form>
+                            <div className="mb-2">
+                                <input type="text" className="form-control" placeholder="Search Names"/>
+                            </div>
+                            <div className="mb-2">
+                                <input type="submit" className="btn btn-outline-dark" value="Search"/>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
