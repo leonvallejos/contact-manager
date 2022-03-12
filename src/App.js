e6e6d5231e7e3d581a0619e9bc5a20c7ca7e9aa3
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-11 23:33:31 -0300

    feat: add route for ViewContact and EditContact for :contactId

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import ContactList from "./components/contacts/ContactList/ContactList";
 import AddContact from "./components/contacts/AddContact/AddContact";
+import ViewContact from "./components/contacts/ViewContact/ViewContact";
+import EditContact from "./components/contacts/EditContact/EditContact";
 
 let App = () => {
     return (
@@ -13,6 +15,8 @@ let App = () => {
                 <Route path={'/'} element={<Navigate to={'/contacts/list'}/>}/>
                 <Route path={'/contacts/list'} element={<ContactList/>}/>
                 <Route path={'/contacts/add'} element={<AddContact/>}/>
+                <Route path={'/contacts/view/:contactId'} element={<ViewContact/>}/>
+                <Route path={'/contacts/edit/:contactId'} element={<EditContact/>}/>
             </Routes>
         </React.Fragment>
     );
