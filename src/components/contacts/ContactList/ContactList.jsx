3fcd057515ea10c0dd0ad0356b919857d947a839
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-13 22:38:38 -0300

    chore: add card, and card body

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -2,42 +2,70 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 let ContactList = () => {
-  return (
-    <React.Fragment>
-      <section className="contact-search p-3">
-        <div className="container">
-            <div className="grid">
-                <div className="row">
-                    <div className="col">
-                        <p className="h3">Contact Manager
-                            <Link to={'/contacts/add'} className="btn btn-primary ms-2">
-                                <i className="fa fa-plus-circle me-2"/>
-                                 New</Link>
-                        </p>
-                        <p className="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium alias aliquam corporis delectus distinctio, dolores eveniet expedita fugiat iusto labore magnam, molestiae quae recusandae tempora vel veniam? Iure, ut.</p>
+    return (
+        <React.Fragment>
+            <section className="contact-search p-3">
+                <div className="container">
+                    <div className="grid">
+                        <div className="row">
+                            <div className="col">
+                                <p className="h3">Contact Manager
+                                    <Link to={'/contacts/add'} className="btn btn-primary ms-2">
+                                        <i className="fa fa-plus-circle me-2"/>
+                                        New</Link>
+                                </p>
+                                <p className="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                                    accusantium alias aliquam corporis delectus distinctio, dolores eveniet expedita
+                                    fugiat iusto labore magnam, molestiae quae recusandae tempora vel veniam? Iure,
+                                    ut.</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <form className="row">
+                                    <div className="col">
+                                        <div className="mb-2">
+                                            <input type="text" className="form-control" placeholder="Search Names"/>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="mb-2">
+                                            <input type="submit" className="btn btn-outline-dark" value="Search"/>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <form className="row">
-                            <div className="col">
-                                <div className="mb-2">
-                                    <input type="text" className="form-control" placeholder="Search Names"/>
+            </section>
+            <section className="contact-list">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="col-md-4">
+                                            <img src="http://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png" alt="user-icon" className="img-fluid contact-img"/>
+                                    </div>
+                                    <div className="col-md-7">
+                                        <ul className="list-group">
+                                            <li className="list-group-item list-group-item-action">
+                                                Name: <span className="fw-bold">John Doe</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-1">
+
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col">
-                                <div className="mb-2">
-                                    <input type="submit" className="btn btn-outline-dark" value="Search"/>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-      </section>
-    </React.Fragment>
-  );
+            </section>
+        </React.Fragment>
+    );
 };
 
 export default ContactList;
