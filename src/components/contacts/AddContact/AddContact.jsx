a754967d5e5f1aa22c29f599cb5b9b310c693e8e
leonvallejos <dev.leonvallejos@gmail.com>
2022-03-17 13:40:09 -0300

    feat: add submit btn, form title, company, email, mobile, photo url, name

--- src/components/contacts/AddContact/AddContact.jsx
+++ src/components/contacts/AddContact/AddContact.jsx
@@ -21,6 +21,29 @@ let AddContact = () => {
                                 <div className="mb-2">
                                     <input type="text" className="form-control" placeholder="Name"/>
                                 </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Photo URL"/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="number" className="form-control" placeholder="Mobile"/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="email" className="form-control" placeholder="Email"/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Company"/>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Title"/>
+                                </div>
+                                <div className="mb-2">
+                                    <select className="form-control">
+                                        <option value="">Select a Group</option>
+                                    </select>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="submit" className="btn btn-success" value="Create"/>
+                                </div>
                             </form>
                         </div>
                     </div>
